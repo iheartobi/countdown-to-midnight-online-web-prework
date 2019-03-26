e85a219445604c4b93fb052f4609dd1e4c8acade
Ititi Obidah <iobidah9@gmail.com>
2019-03-26 20:35:31 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,10 @@
 #write your code here
 
-def countdown
+def countdown(number)
+  counter = 10 
+  while counter > 0 
+  puts "#{number} SECOND(S)!"
+  counter -= 1
 end
+return "HAPPY NEW YEAR!"
+ end
