3a38668bb3b04cafe649424a76e464af58c9c5fe
Ititi Obidah <iobidah9@gmail.com>
2019-03-26 20:59:35 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -10,6 +10,12 @@ end
 return "HAPPY NEW YEAR!"
  end
 
-def countdown_with_sleep (num, nap)
-  
+def countdown_with_sleep (num)
+  num = 10 
+  while num > 0  
+  puts "#{num} SECOND(S)!"
+  num -= 1 
+  sleep ()
+end 
+  return "HAPPY NEW YEAR!"
 end 
